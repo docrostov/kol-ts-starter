3bd5258189a8e0367d7ad0329670f6529d58b86b
Aaron McGuire <aaron.mcguire@2OS.com>
2020-12-16 13:27:22 -0500

    pushing latest

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+export function breakfast() {
+  setClan('Bonus Adventures from Hell');
+}
+
+export function main() {
+  breakfast();
+  print("Hey, you ran a script! Cool!");
+}
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var path = require('path');
+var webpack = require('webpack');
+var packageData = require('./package.json');
+
+module.exports = {
+  entry: {
+    // Point "entry" to scripts you want to be CLI-eligible.
+    breakfast: './src/main.ts',
+  },
+  mode: 'development',
+  devtool: false,
+  output: {
+    // Change the final string here to the name you want your script to use in mafia.
+    path: path.resolve(__dirname, 'KoLmafia', 'scripts', packageData.name),
+    filename: '[name].js',
+    libraryTarget: 'commonjs',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        // Include ts, tsx, js, and jsx files.
+        test: /\.(ts|js)x?$/,
+        // exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [],
+  externals: {
+    kolmafia: 'commonjs kolmafia',
+    'canadv.ash': 'commonjs canadv.ash',
+  },
+};
