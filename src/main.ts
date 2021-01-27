0524236b5c7c0c77300d8ab3d57cf703306cc552
Patrick Hulin <patrick.hulin@gmail.com>
2021-01-26 22:53:10 -0500

    Update README and format main.ts.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
-import {
-  myMp,
-  print,
-} from 'kolmafia';
+import { myMp, print } from "kolmafia";
 
 export function checkMP() {
-  if( myMp() < 200 ){
-    return "Your MP is less than 200."
+  if (myMp() < 200) {
+    return "Your MP is less than 200.";
   } else {
-    return "Your MP is greater than or equal to 200."
+    return "Your MP is greater than or equal to 200.";
   }
 }
 
